96b6c7b0180be7cb60aa544e9fa6f0f20a2bcda9
srbhkumar624 <srbhkumar624@gmail.com>
2022-08-23 12:49:32 +0530

    initialProjectSetup

---
+++ src/ContextApi/ContextApp.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ContextApp = () => {
+  return (
+    <div>ContextApp</div>
+  )
+}
+
+export default ContextApp
---
+++ src/ContextApi/PrivateContext.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const PrivateContext = () => {
+  return (
+    <div>PrivateContext</div>
+  )
+}
+
+export default PrivateContext
